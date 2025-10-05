8af48e70ccd3f48a036c9d57a72fdffcd935c3ff
qhardwick <qhardwick@gmail.com>
2025-10-05 18:32:47 -0400

    implemented processor

--- src/main/java/com/revature/configs/StepConfig.java
+++ src/main/java/com/revature/configs/StepConfig.java
@@ -7,9 +7,11 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -21,11 +23,22 @@ import java.util.Map;
 @Configuration
 public class StepConfig {
 
+    // Provided by SpringBatch:
+    JobRepository jobRepository;
+    PlatformTransactionManager transactionManager;
+
+    @Autowired
+    public StepConfig(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        this.jobRepository = jobRepository;
+        this.transactionManager = transactionManager;
+    }
+
     @Bean(name = "updateUsersStep")
-    public Step updateUsersStep(JpaPagingItemReader<User> userReader, ItemWriter<User> userWriter, JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+    public Step updateUsersStep(JpaPagingItemReader<User> userReader, ItemProcessor<User, User> userProcessor, ItemWriter<User> userWriter, JobRepository jobRepository) {
         return new StepBuilder("updateUsersStep", jobRepository)
                 .<User, User>chunk(10, transactionManager)
                 .reader(userReader)
+                .processor(userProcessor)
                 .writer(userWriter)
                 .faultTolerant()
                 .retryLimit(3)
@@ -33,6 +46,7 @@ public class StepConfig {
                 .build();
     }
 
+    // The reader is where we fetch the data we wish to process:
     @Bean
     @StepScope
     public JpaPagingItemReader<User> userReader(@Value("#{jobParameters['year']}") String year, EntityManagerFactory entityManagerFactory) {
@@ -45,11 +59,21 @@ public class StepConfig {
                 .build();
     }
 
+    // The processor is where we transform or enrich each element in our data collection. Note: if you need to send the entire collection to an
+    // external api, you might want to do that in the writer to avoid making a separate call for each entity. Here, we are taking each User object
+    // and updating it, so it comes in as a User and exits as a User, hence ItemProcessor<User, User>:
+    @Bean
+    public ItemProcessor<User, User> userProcessor() {
+        return user -> {
+            user.setUsername("User" + user.getId());
+            return user;
+        };
+    }
+
+    // The writer is where we persist our changes for each chunk. Note that chunk does not necessarily equal page size. Page is how many rows you pull out at a time,
+    // chunk is how many you process for each commit:
     @Bean
     public ItemWriter<User> userWriter(UserRepository userRepository) {
-        return users -> {
-            users.getItems().forEach(user -> user.setUsername("User" + user.getId()));
-            userRepository.saveAllAndFlush(users.getItems());
-        };
+        return users -> userRepository.saveAll(users.getItems());
     }
 }
